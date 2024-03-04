7482648e9b73b8a818278539309c131abd0480e1
Mickey Langley <mickeylangley@comcast.net>
2024-03-04 01:12:40 -0500

    Update Program.cs
    committed from GitHub Desktop

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -2,3 +2,4 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("I just added this to Visual Studio");
 Console.WriteLine("I just added this in the command line repo");
+Console.WriteLine("This line added to show the stagin area");
